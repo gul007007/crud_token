4a10d51d8e055f878ada60b5ef6e1fd7c3a10b74
Muhammad Faisal Gul <faisalgull3.o@gmail.com>
2025-07-04 21:09:46 +0500

    new backend code

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ const app = express();
 
 app.use(express.json());
 // add frontend URL
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://crud-frontend-kohl-alpha.vercel.app/", credentials: true }));
 app.use(cookieParser());
 
 connectDB()
